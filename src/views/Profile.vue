ac3f75405fae4a711c832de0161effde013c0cc0
atiqohh <atiqohnazilatun@gmail.com>
2021-10-19 12:43:59 +0700

    first commit

---
+++ src/views/Profile.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="profile">
+    <h1>This is a profile page</h1>
+  </div>
+</template>
+
+<script>
+import 'bootstrap/dist/css/bootstrap.css'
+
+export default {
+  name: 'Profile',
+  components: {}
+}
+</script>
